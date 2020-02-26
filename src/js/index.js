f832549b00a8bf81f12da00cdfb8661aabfb36a4
Shakhriyar <56295240+sidzha@users.noreply.github.com>
2020-02-26 17:55:28 +0300

    v 0.2
    👷‍♂️ WIP
    * tablet - 90%

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,16 @@
-const $div1 = $('.page-main-2__features'),
-  $div2 = $('.page-main-2__price'),
-  currentDiv = 'div1',
-  $button = $('.page-main-2__button');
+let $div1 = $(".page-main-2__features"),
+  $div2 = $(".page-main-2__price"),
+  currentDiv = "div1",
+  $button = $(".theme-switch__input"),
+  $firstSpan = $(".js-first"),
+  $secondSpan = $(".js-second");
 
 $div2.hide();
 
-$(document).on('click', 'button', function(evt) {
-  $div1.toggle('slide', { direction: 'left' }, 'slow');
-  $div2.toggle('slide', { direction: 'right' }, 'slow');
-
-  currentDiv = currentDiv === 'div1' ? 'div2' : 'div1';
+$($button).on("click", function() {
+  $div1.toggle("slide", { direction: "left" }, "medium");
+  $div2.toggle("slide", { direction: "right" }, "medium");
+  currentDiv = currentDiv === "div1" ? "div2" : "div1";
+  $firstSpan.toggleClass("dark");
+  $secondSpan.toggleClass("light");
 });
